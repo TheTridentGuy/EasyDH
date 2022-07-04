bf38a68d535e50be5a4529080341319a18c6b61a
TheTridentGuy <99673331+TheTridentGuy@users.noreply.github.com>
2022-07-04 09:20:45 -0700

    New Version PWA

--- sw.js
+++ sw.js
@@ -1,51 +1,25 @@
-self.addEventListener("install", function(event) {
-    event.waitUntil(preLoad());
-  });
-  
-  var preLoad = function(){
-    console.log("Installing web app");
-    return caches.open("offline").then(function(cache) {
-      console.log("caching index and important routes");
-      return cache.addAll(["/", "/index.html", "/offline.html", "/CryptoJS"]);
-    });
-  };
-  
-  self.addEventListener("fetch", function(event) {
-    event.respondWith(checkResponse(event.request).catch(function() {
-      return returnFromCache(event.request);
-    }));
-    event.waitUntil(addToCache(event.request));
-  });
-  
-  var checkResponse = function(request){
-    return new Promise(function(fulfill, reject) {
-      fetch(request).then(function(response){
-        if(response.status !== 404) {
-          fulfill(response);
-        } else {
-          reject();
-        }
-      }, reject);
-    });
-  };
-  
-  var addToCache = function(request){
-    return caches.open("offline").then(function (cache) {
-      return fetch(request).then(function (response) {
-        console.log(response.url + " was cached");
-        return cache.put(request, response);
-      });
-    });
-  };
-  
-  var returnFromCache = function(request){
-    return caches.open("offline").then(function (cache) {
-      return cache.match(request).then(function (matching) {
-       if(!matching || matching.status == 404) {
-         return cache.match("offline.html");
-       } else {
-         return matching;
-       }
-      });
-    });
-  };
+// Change this to your repository name
+var GHPATH = '/EasyDH';
+ 
+// Choose a different app prefix name
+var APP_PREFIX = 'EasyDH';
+ 
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+ 
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/offline.html`,
+  `${GHPATH}/manifest.json`,
+  `${GHPATH}/sw.js`,
+  `${GHPATH}/CryptoJS/rollups/aes.js`,
+  `${GHPATH}/ezdh180.png`,
+  `${GHPATH}/ezdh192.png`,
+  `${GHPATH}/favicon.ico`
+]
