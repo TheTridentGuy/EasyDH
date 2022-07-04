7f24635f1e0c9481ca74b9019e1aba60c966042e
TheTridentGuy <99673331+TheTridentGuy@users.noreply.github.com>
2022-07-04 09:48:48 -0700

    PWA Update 02

--- sw.js
+++ sw.js
@@ -1,6 +1,3 @@
-// Change this to your repository name
-var GHPATH = '/EasyDH';
- 
 // Choose a different app prefix name
 var APP_PREFIX = 'EasyDH';
  
@@ -8,20 +5,20 @@ var APP_PREFIX = 'EasyDH';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_01';
+var VERSION = 'version_02';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
-  `${GHPATH}/`,
-  `${GHPATH}/index.html`,
-  `${GHPATH}/offline.html`,
-  `${GHPATH}/manifest.json`,
-  `${GHPATH}/sw.js`,
-  `${GHPATH}/CryptoJS/rollups/aes.js`,
-  `${GHPATH}/ezdh180.png`,
-  `${GHPATH}/ezdh192.png`,
-  `${GHPATH}/favicon.ico`
+  `/`,
+  `/index.html`,
+  `/offline.html`,
+  `/manifest.json`,
+  `/sw.js`,
+  `/CryptoJS/rollups/aes.js`,
+  `/ezdh180.png`,
+  `/ezdh192.png`,
+  `/favicon.ico`
 ]
 
 var CACHE_NAME = APP_PREFIX + VERSION
